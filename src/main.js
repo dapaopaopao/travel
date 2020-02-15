e6e966974e8df2671253b4cfbe5bfbfec8cfe21a
dapaopaopao <381281999@qq.com>
2020-02-16 01:31:57 +0800

    '2-16'

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './assets/reset.css'
+import './assets/border.css'
+import fastClick from 'fastclick'
+import router from './router'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>welcome home</div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style scoped>
+</style>
